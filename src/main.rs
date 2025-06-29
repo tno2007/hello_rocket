9bd370e00ec16b6f1ef8f21fd94690f7b9b6ee3f
Lyall <lyall@iriesoft.net>
2025-06-29 10:49:45 +0200

    up

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 #[macro_use] extern crate rocket;
 
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/hello/<name>")]
+fn hello(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
 #[get("/<name>/<age>")]
-fn hello(name: &str, age: u8) -> String {
+fn helloMultiple(name: &str, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/hello", routes![hello])
+    rocket::build().mount("/", routes![index, hello, helloMultiple])
 }
